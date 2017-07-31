f16f64b5b4bcbbfeaff7f06dfd217720e5ca31da
Your Correct Name <your-correct-email@example.com>
2017-07-31 17:26:02 +1000

    Created UT for ObjectFactory

--- src/main/java/rea/toyrobot/factories/ObjectFactory.java
+++ src/main/java/rea/toyrobot/factories/ObjectFactory.java
@@ -3,10 +3,24 @@ package rea.toyrobot.factories;
 /**
  * When given a class the object factory creates a instance of that object
  * assigning it to the base interface that it needs to be.
+ *
+ * The class should be sub classed by a concrete object factory that produces
+ * something more concrete.
+ *
+ * The concrete implementations of this should looks something like
+ *
+ *  WorldObjectFactory<C> extends ObjectFactory<World, C extends World>
+ *
+ *  where World is a interface.
  */
 
-public class ObjectFactory<I, C> {
-    protected I createObject(Class<C> clazz) {
+public abstract class ObjectFactory<I, C> {
+
+    /**
+     *
+     * @return a blank instance of a given object.
+     */
+    protected I createObject(final Class<C> oClass) throws InstantiationException, IllegalAccessException {
         return null;
     }
 }
--- src/test/java/rea/toyrobot/factories/TestObjectFactory.java
+++ src/test/java/rea/toyrobot/factories/TestObjectFactory.java
@@ -2,10 +2,28 @@ package rea.toyrobot.factories;
 
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class TestObjectFactory {
 
-    @Test
-    public void testGenerateObject() {
+    private interface of1Iface {
+       void someMethod();
+    }
 
+    private class if1concrete implements of1Iface {
+        @Override
+        public void someMethod() {
+
+        }
+    }
+
+    private ObjectFactory<of1Iface, if1concrete> objectFactory = new ObjectFactory() {};
+
+    @Test
+    public void testGenerateObject() throws Exception {
+        of1Iface concrete = objectFactory.createObject(if1concrete.class);
+
+        assertThat(concrete.getClass().toString(), is("if1concrete"));
     }
 }
