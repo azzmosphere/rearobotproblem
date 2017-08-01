99b647817bdae4714a711bcd00d341bf2d532333
Your Correct Name <your-correct-email@example.com>
2017-08-01 20:36:40 +1000

    Added unit tests for compass configuration.

--- src/test/java/rea/toyrobot/config/mapper/TestCompassConfig.java
+++ src/test/java/rea/toyrobot/config/mapper/TestCompassConfig.java
@@ -1,4 +1,17 @@
 package rea.toyrobot.config.mapper;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 public class TestCompassConfig {
+    private RobotCompassConfig robotCompassConfig = new RobotCompassConfig();
+
+    @Test
+    public void testRobotCompassConfig() {
+        robotCompassConfig.setInitialpos("NORTH");
+
+        assertThat(robotCompassConfig.getInitialpos(), is("NORTH"));
+    }
 }
