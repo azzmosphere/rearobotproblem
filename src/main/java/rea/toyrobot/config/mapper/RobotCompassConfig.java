73264c1b59c48f6756f85c299b918c77ca5d4e9a
Your Correct Name <your-correct-email@example.com>
2017-08-01 20:38:03 +1000

    completed implementation of compass configuration.

--- src/main/java/rea/toyrobot/config/mapper/RobotCompassConfig.java
+++ src/main/java/rea/toyrobot/config/mapper/RobotCompassConfig.java
@@ -3,12 +3,14 @@ package rea.toyrobot.config.mapper;
 import javax.xml.bind.annotation.XmlElement;
 
 public class RobotCompassConfig {
+    private String initpos;
+
     @XmlElement
     public void setInitialpos(String initialpos) {
-
+        this.initpos = initialpos;
     }
 
     public String getInitialpos() {
-        return null;
+        return initpos;
     }
 }
