21e03d8b8f416a353124e29c9dd73bc3d919bef1
Your Correct Name <your-correct-email@example.com>
2017-08-01 21:51:35 +1000

    completed implementation of robot world view.

--- src/main/java/rea/toyrobot/perspective/RobotWorldView.java
+++ src/main/java/rea/toyrobot/perspective/RobotWorldView.java
@@ -1,33 +1,36 @@
 package rea.toyrobot.perspective;
 
 public class RobotWorldView implements RobotWorldPerspective {
+    private int x, y;
+    PerspectiveCompass compass;
+
     @Override
     public int getXPos() {
-        return 0;
+        return x;
     }
 
     @Override
     public void setXPos(int x) {
-
+        this.x = x;
     }
 
     @Override
     public int getYpos() {
-        return 0;
+        return y;
     }
 
     @Override
     public void setYPos(int y) {
-
+        this.y = y;
     }
 
     @Override
     public PerspectiveCompass getCompass() {
-        return null;
+        return compass;
     }
 
     @Override
     public void setCompass(PerspectiveCompass compass) {
-
+        this.compass = compass;
     }
 }
