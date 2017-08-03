20e112d06500cbfae99b6041113c76896f1661ce
Your Correct Name <your-correct-email@example.com>
2017-08-04 09:59:35 +1000

    Added unit tests for LeftAction.

--- src/test/java/rea/toyrobot/actions/physicalobjects/TestLeftAction.java
+++ src/test/java/rea/toyrobot/actions/physicalobjects/TestLeftAction.java
@@ -1,4 +1,34 @@
 package rea.toyrobot.actions.physicalobjects;
 
+import org.junit.Before;
+import org.junit.Test;
+import rea.toyrobot.perspective.Perspective;
+import rea.toyrobot.perspective.PerspectiveCompass;
+import rea.toyrobot.physicalobjects.PhysicalObject;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+
 public class TestLeftAction {
+    private LocalAction leftAction = new LeftAction();
+    private PhysicalObject robot = mock(PhysicalObject.class);
+    private Perspective perspective = mock(Perspective.class);
+    private PerspectiveCompass compass = mock(PerspectiveCompass.class);
+
+    @Before
+    public void init() throws Exception {
+        when(robot.getPerspective()).thenReturn(perspective);
+        when(perspective.getCompass()).thenReturn(compass);
+        leftAction.setPhysicalObject(robot);
+    }
+
+    @Test
+    public void testMoveLeft() throws Exception {
+        leftAction.runAction();
+        verify(compass, atLeastOnce()).rotateAntiClockwise();
+        verify(compass, atMost(1)).rotateAntiClockwise();
+    }
 }
