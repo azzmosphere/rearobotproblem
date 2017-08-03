21a30b70c9df13b1a7e1a24ddc0bd690cb5a6f46
Your Correct Name <your-correct-email@example.com>
2017-08-03 19:36:15 +1000

    completed implementation of Place action

--- src/main/java/rea/toyrobot/actions/world/PlaceAction.java
+++ src/main/java/rea/toyrobot/actions/world/PlaceAction.java
@@ -1,38 +1,78 @@
 package rea.toyrobot.actions.world;
 
+/**
+ * The PlaceAction places a physical object in the world and adjusts that
+ * objects perspective to match its position in the world.
+ */
+
 import rea.toyrobot.exceptions.RobotException;
+import rea.toyrobot.exceptions.RobotInitialisationException;
 import rea.toyrobot.physicalobjects.PhysicalObject;
 import rea.toyrobot.physicalobjects.PhysicalObjectFactory;
 import rea.toyrobot.worlds.World;
 
 public class PlaceAction implements WorldAction {
+    private String[] argsIn = null;
+    private PhysicalObjectFactory physicalObjectFactory;
+    private World world;
+    private String response = null;
+
     @Override
     public void setWorld(World world) {
-
+        this.world = world;
     }
 
     @Override
     public PhysicalObject runAction() throws RobotException {
-        return null;
+        PhysicalObject physicalObject = null;
+        try {
+            String[] args = argsIn[1].split(",");
+            int x = Integer.parseInt(args[0]), y = Integer.parseInt(args[1]);
+
+            if (world.canMoveTo(x, y)) {
+                world.setObject(x, y);
+
+                physicalObject = physicalObjectFactory.create();
+                physicalObject.getPerspective().setXPos(x);
+                physicalObject.getPerspective().setYPos(y);
+                physicalObject.getPerspective().getCompass().findCardinalDirection(args[2]);
+            }
+            else {
+                response = "unable to place object at position " + x + ":" + y;
+            }
+        }
+        catch (RobotException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new RobotInitialisationException("an unchecked error has occurred :" + e.getMessage());
+        }
+
+
+        return physicalObject;
     }
 
     @Override
     public void setArgsIn(String[] argsIn) {
-
+        this.argsIn = argsIn;
     }
 
     @Override
     public String[] getResponse() {
-        return new String[0];
+        return new String[] {response};
     }
 
     @Override
     public void setPhysicalObjectFactory(PhysicalObjectFactory physicalObjectFactory) {
-
+        this.physicalObjectFactory = physicalObjectFactory;
     }
 
     @Override
     public boolean canPerformAction(String[] argsIn) {
-        return false;
+        boolean rv = false;
+        if (argsIn.length > 0 && argsIn[0] != null && "PLACE".equalsIgnoreCase(argsIn[0])) {
+            rv = true;
+        }
+        return rv;
     }
 }
--- src/test/java/rea/toyrobot/actions/world/TestPlaceAction.java
+++ src/test/java/rea/toyrobot/actions/world/TestPlaceAction.java
@@ -2,45 +2,53 @@ package rea.toyrobot.actions.world;
 
 import org.junit.Before;
 import org.junit.Test;
+import rea.toyrobot.perspective.Perspective;
+import rea.toyrobot.perspective.PerspectiveCompass;
 import rea.toyrobot.physicalobjects.PhysicalObject;
 import rea.toyrobot.physicalobjects.PhysicalObjectFactory;
 import rea.toyrobot.worlds.World;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doNothing;
 
 public class TestPlaceAction {
     private WorldAction placeAction = new PlaceAction();
     private PhysicalObject robot = mock(PhysicalObject.class);
     private World tableTop = mock(World.class);
     private PhysicalObjectFactory factory = mock(PhysicalObjectFactory.class);
+    private Perspective perspective = mock(Perspective.class);
+    private PerspectiveCompass perspectiveCompass = mock(PerspectiveCompass.class);
 
     @Before
     public void init() throws Exception {
         placeAction.setWorld(tableTop);
         placeAction.setPhysicalObjectFactory(factory);
 
-        when(tableTop.canMoveTo(0, 0)).thenReturn(true);
-        doNothing().when(tableTop).setObject(0, 0);
         when(factory.create()).thenReturn(robot);
+        when(robot.getPerspective()).thenReturn(perspective);
+        when(perspective.getCompass()).thenReturn(perspectiveCompass);
     }
 
     @Test
     public void testCanPerformAction() {
-        assertThat(placeAction.canPerformAction(new String[] {"PLACE", "0", "0", "NORTH"}), is(true));
+        assertThat(placeAction.canPerformAction(new String[] {"PLACE", "0,0,NORTH"}), is(true));
         assertThat(placeAction.canPerformAction(new String[] {"LEFT"}), is(false));
     }
 
     @Test
     public void testRunAction() throws Exception {
-        placeAction.setArgsIn(new String[] {"PLACE", "0", "0", "NORTH"});
+        doNothing().when(tableTop).setObject(0, 0);
+        doNothing().when(perspective).setXPos(0);
+        doNothing().when(perspective).setYPos(0);
+        when(tableTop.canMoveTo(0, 0)).thenReturn(true);
+
+        placeAction.setArgsIn(new String[] {"PLACE", "0,0,NORTH"});
         PhysicalObject object = placeAction.runAction();
 
-        assertThat(object.getPerspective().getXPos(), is(0));
-        assertThat(object.getPerspective().getYpos(), is(0));
-        assertThat(object.getPerspective().getCompass().getCardinalDirection(), is("NORTH"));
-        assertThat(tableTop.canMoveTo(0, 0), is(false));
+        assertThat(object, is(robot));
     }
 
 }
