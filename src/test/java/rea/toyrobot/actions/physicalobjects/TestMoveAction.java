288500990e1d6eecbb8b40996f51dc47bb670655
Your Correct Name <your-correct-email@example.com>
2017-08-03 21:48:00 +1000

    created initial test

--- src/test/java/rea/toyrobot/actions/physicalobjects/TestMoveAction.java
+++ src/test/java/rea/toyrobot/actions/physicalobjects/TestMoveAction.java
@@ -4,12 +4,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import rea.toyrobot.perspective.Perspective;
+import rea.toyrobot.perspective.PerspectiveCompass;
 import rea.toyrobot.physicalobjects.PhysicalObject;
 import rea.toyrobot.worlds.World;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 public class TestMoveAction {
     private GlobalAction moveAction = new MoveAction();
@@ -17,20 +20,24 @@ public class TestMoveAction {
     private PhysicalObject physicalObjectMock = mock(PhysicalObject.class);
     private Perspective perspective = mock(Perspective.class);
     private World world = mock(World.class);
+    private PerspectiveCompass perspectiveCompass = mock(PerspectiveCompass.class);
 
     @Before
     public void init() {
         moveAction.setPhysicalObject(physicalObjectMock);
         moveAction.setWorld(world);
         when(physicalObjectMock.getPerspective()).thenReturn(perspective);
+        when(perspective.getXPos()).thenReturn(1);
+        when(perspective.getYpos()).thenReturn(1);
+
+        perspective.setCompass(perspectiveCompass);
     }
 
     @Test
     public void testRunActionNorth() throws Exception {
-        when(perspective.getCompass().getCardinalDirection()).thenReturn("NORTH");
-
+        when(perspectiveCompass.getCardinalDirection()).thenReturn("NORTH");
         moveAction.runAction();
-
         verify(perspective).setYPos(yarg.capture());
+        assertThat(yarg.getValue(), is(2));
     }
 }
