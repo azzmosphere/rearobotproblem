0cd8f3c20430915a6d1c22e4f8b9261cd8c80c1b
Your Correct Name <your-correct-email@example.com>
2017-08-07 15:34:56 +1000

    completed implementation for responder.

--- src/main/java/rea/toyrobot/responder/ReaRobotResponder.java
+++ src/main/java/rea/toyrobot/responder/ReaRobotResponder.java
@@ -1,23 +1,27 @@
 package rea.toyrobot.responder;
 
 public class ReaRobotResponder implements RobotResponder {
+
+    private boolean hasResponse;
+    private String response;
+
     @Override
     public boolean hasResponse() {
-        return false;
+        return hasResponse;
     }
 
     @Override
     public void setHasResponse(boolean hasResponse) {
-
+        this.hasResponse = hasResponse;
     }
 
     @Override
     public String getResponse() {
-        return null;
+        return response;
     }
 
     @Override
     public void setResponse(String response) {
-
+        this.response = response;
     }
 }
