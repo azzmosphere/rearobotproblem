58f05618cd46dfe501eef1e4ba19b262b3f57eda
Your Correct Name <your-correct-email@example.com>
2017-08-07 13:10:08 +1000

    completed implementation for ToyRobot

--- src/main/java/rea/toyrobot/physicalobjects/ToyRobotFactory.java
+++ src/main/java/rea/toyrobot/physicalobjects/ToyRobotFactory.java
@@ -1,16 +1,34 @@
 package rea.toyrobot.physicalobjects;
 
 import rea.toyrobot.exceptions.RobotInitialisationException;
+import rea.toyrobot.factories.ReaObjectFactory;
 import rea.toyrobot.perspective.PerspectiveFactory;
 
-public class ToyRobotFactory implements PhysicalObjectFactory {
+public class ToyRobotFactory extends ReaObjectFactory<PhysicalObject, ToyRobot> implements PhysicalObjectFactory {
+    PerspectiveFactory perspectiveFactory;
+
     @Override
     public void setPerspectiveFactory(PerspectiveFactory perspectiveFactory) {
+        this.perspectiveFactory = perspectiveFactory;
+    }
 
+    public ToyRobotFactory() {
+        super(ToyRobot.class);
     }
 
     @Override
-    public PhysicalObject create() throws RobotInitialisationException {
-        return null;
+    protected ToyRobot init(ToyRobot object) throws RobotInitialisationException {
+
+        try {
+            object.setPerspective(perspectiveFactory.create());
+        }
+        catch (RobotInitialisationException e) {
+            throw e;
+        }
+        catch (Exception e) {
+            throw new RobotInitialisationException("a unchecked error has occurred " + e.getMessage());
+        }
+
+        return object;
     }
 }
